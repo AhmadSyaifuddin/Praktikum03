4c32b48dbab6e9e168bbd112e900ba5940c27a09
AhmadSyaifuddin <32382801+AhmadSyaifuddin@users.noreply.github.com>
2017-11-06 13:29:54 +0800

    menghitung tinggi badan

--- underweight_ normal_overweight.cpp
+++ underweight_ normal_overweight.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main() 
 {
-	
+	cout<<"PROGRAM MENENTUKAN TINGGI BADAN"<<endl;
+    float b;
+    int t;
+    
+    cout << "Silahkan masukkan tinggi badan:  "; cin >>t;
+    cout << "Silahkan masukkan berat badan : ";
+    cin >> b;
+    
+    if (b<t/2.5)
+       cout << "jawaban :Underweight" << endl;
+            else if (t/2.3 < b)
+                 cout <<  " Overweight" << endl;
+            else if (t/2.5 <= b <= t/2.3)
+           		 cout << "jawaban : Normal" << endl;
+                 
+                 
+                 return 0;
     }
